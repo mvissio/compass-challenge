a85e7e054aa288e7f29666043b972a979e2f8371
Marcos Vissio <marcos.vissio@gmail.com>
2026-09-19 18:51:29 -0300

    Chore: First commit

---
+++ src/main/java/com/compass/challenge/CompassChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.compass.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompassChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compass/challenge/CompassChallengeApplicationTest.java
@@ -0,0 +1,4 @@
+package com.compass.challenge;
+
+public class CompassChallengeApplicationTest {
+}
